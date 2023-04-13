c22a339d3cd3d108fe8babe5a9ee9de5c62f0659
torippy <47879788+torippy1024@users.noreply.github.com>
2023-04-13 20:52:32 +0900

    dotenvに型を付ける

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import hello from './hello';
 
 global.hello = hello;
+
+global.printPrivateVariable = () => {
+  const secretVariale = process.env.SECRET_VARIABLE;
+  console.log(secretVariale);
+};
